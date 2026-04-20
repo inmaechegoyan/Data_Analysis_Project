d708737be1c91b90551a8652b95759a3f1acf2ea
Inmaculada Echegoyan <s253701@dtu.dk>
2026-04-20 13:23:55 +0200

    Change in ex11

---
+++ src/exercise_11-13.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+# Import the class and function 
+
+from data.clean_data_class import People
+from src.read_people_info import read_people_info
+
+#####################
+#### EXERCISE 11 ###
+####################
+
+
+for person in read_people_info('data/people.db'):
+    continue
+    
+
+
+#####################
+#### EXERCISE 12 ###
+####################
+
+# Do tall people marry (or at least get children together)? To answer that, calculate the percentages of tall/tall, tall/normal, 
+# tall/short, normal/normal, normal/short, and short/short couples. Decide your own limits for tall, normal and short, and if 
+# they are the same for men and women.
+
+
+#####################
+#### EXERCISE 13 ###
+####################
+
+# Do tall parents get tall children?
