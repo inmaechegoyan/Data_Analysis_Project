0508b2eef101f574445fb4162627c94e948cb551
Inmaculada Echegoyan <s253701@dtu.dk>
2026-04-27 16:16:36 +0200

    progess

--- src/script_final_inma.py
+++ src/script_final_inma.py
@@ -27,10 +27,9 @@ age_count_father = 0
 total_father = 0
 
 
-
 ##### QUESTION 3 #####
 
-#age_intervals = [(15, 19), (20, 24), (25, 29), (30, 34), (35, 39), (40, 99)]    # PODRIAMOS USAR EL MISMO QUE EN EL 1??? 
+father_counts = [0] * len(age_intervals)
 
 ##### QUESTION 4 #####
 
@@ -41,14 +40,27 @@ age_count_mother = 0
 
 ##### QUESTION 5 #####.     TODAS ESTAN INCLUIDAS EN OTRAS ACT (LO QUE NO SE ES SI HABRA QUE RESTART ALGUNAS VARIABLES DURANTE EL CODE)
 
+mother_counts = [0] * len(age_intervals)
+
 ##### QUESTION 6 #####
 
 woman_without_children = 0
 men_without_children = 0
 
+##### QUESTION 7 #####
+child_to_parents = dict()
+age_difference = []
+
+##### QUESTION 8 #####
+people_with_grandparents = 0
+total_people = 0
+
+
+
+
 
 for person in read_people_info('data/people.db'):   # THIS CAN ONLY APPEAR ONE IN THE SCRIPT!!!!!!!
-
+    person_cpr = person.cpr
     # Q1
     person_age = person.age
     person_gender = person.gender
@@ -69,7 +81,7 @@ for person in read_people_info('data/people.db'):   # THIS CAN ONLY APPEAR ONE I
         age_count_father += age_first_child
         total_father += 1
         if age_first_child > max_age_father:
-            max_age = age_first_child
+            max_age_father = age_first_child
         if age_first_child < min_age_father:
             min_age_father = age_first_child
 
@@ -77,15 +89,12 @@ for person in read_people_info('data/people.db'):   # THIS CAN ONLY APPEAR ONE I
     if person.gender == "M":
         age = person.age_first_child()
         if age is not None:
-            total_father += 1
-
             for i, (low, high) in enumerate(age_intervals):
                 if low <= age <= high:
-                    male_counts[i] += 1
+                    father_counts[i] += 1
                     break
             
     # Q4
-    age_first_child = person.age_first_child()
     if person.gender == "F" and age_first_child is not None:
         age_count_mother += age_first_child
         total_mother += 1
@@ -95,34 +104,64 @@ for person in read_people_info('data/people.db'):   # THIS CAN ONLY APPEAR ONE I
             min_age_mother = age_first_child
     
 
-    # # Q5 y Q6
-    # if person.gender == "F":
-    #     total_female += 1
-    #     age = person.age_first_child()
-    #     if age is not None:
-    #         total_mother += 1
-
-    #         for i, (low, high) in enumerate(age_intervals):
-    #             if low <= age <= high:
-    #                 female_counts[i] += 1
-    #                 break
-    #     if person.children == []:
-    #         woman_without_children += 1
-    # else: 
-    #     total_male += 1
-    #     if person.children == []:
-    #         men_without_children += 1
-
-    # Q5
+    # Q5 y Q6
     if person.gender == "F":
         age = person.age_first_child()
         if age is not None:
-            # total_mother += 1
 
             for i, (low, high) in enumerate(age_intervals):
                 if low <= age <= high:
-                    # female_counts[i] += 1
+                    mother_counts[i] += 1
                     break
+        if person.children == []:
+            woman_without_children += 1
+    else: 
+        if person.children == []:
+            men_without_children += 1
+
+
+    # Create dict child_to_parents
+    for child in person.children: 
+        if child not in child_to_parents: 
+            child_to_parents[child] = []
+        child_to_parents[child].append(person)
+
+
+    # Q8
+
+total_people += 1
+
+# Find the parents: 
+parents = child_to_parents.get(person.cpr, [])
+
+# Find the grandparetns 
+has_grandparents = False
+
+for parent in parents : 
+    grandparents = child_to_parents.get(parent.cpr, [])
+    if len(grandparents) > 0:  
+        has_grandparents = True
+        break
+if has_grandparents: 
+    people_with_grandparents += 1
+
+
+
+
+# Q7 
+for parents in child_to_parents.values(): 
+    if len(parents) >= 2: 
+        for i in range(len(parents)):
+            for j in range(i+1, len(parents)): 
+                diff = abs(parents[i].age - parents[j].age)
+                age_difference.append(diff)
+    
+if len(age_difference) > 0: 
+    avg_difference = sum(age_difference) / len(age_difference)
+else: 
+    avg_difference = 0
+
+
 
 
 
@@ -135,12 +174,15 @@ for person in read_people_info('data/people.db'):   # THIS CAN ONLY APPEAR ONE I
 # Q2
 avg_age_father = age_count_father / total_father
 
-# Q4
+# # Q4
 avg_age_mother = age_count_mother / total_mother
 
 # Q6 
 percent_women = (woman_without_children / total_female) * 100
 percent_men = (men_without_children / total_male) * 100
+
+# Q8
+percentage_grandparents = (people_with_grandparents/total_people) * 100
 
 
 
@@ -169,7 +211,7 @@ print(f"Average age of first-time fathers::{avg_age_father:.2f}")
 print(f"{'Age range':<10} {'Male %':<10}")
 
 for i, (low, high) in enumerate(age_intervals):
-    male_percentage = (male_counts[i] / total_father) * 100 if total_father > 0 else 0
+    male_percentage = (father_counts[i] / total_father) * 100 if total_father > 0 else 0
     print(f"{low}-{high:<8} {male_percentage:<10.2f}") 
 
 
@@ -184,9 +226,16 @@ print(f"Average age of first-time mothers:{avg_age_mother:.2f}")
 print(f"{'Age range':<10} {'Female %':<10}")
 
 for i, (low, high) in enumerate(age_intervals):
-    female_percentage = (female_counts[i] / total_mother) * 100 if total_mother > 0 else 0
+    female_percentage = (mother_counts[i] / total_mother) * 100 if total_mother > 0 else 0
     print(f"{low}-{high:<8} {female_percentage:<10.2f}") 
 
 # Q6
 print(f"Women without children: {percent_women:.2f}%")
 print(f"Men without children: {percent_men:.2f}%")
+
+# Q7
+
+print(f'The age difference between the parents with a common kid is {avg_difference:.2f} years')
+
+# Q8 
+print(f'The number of people that has at least one grandparent is {people_with_grandparents} which correspnd to the {percentage_grandparents:.2f}% of the people in the database')
